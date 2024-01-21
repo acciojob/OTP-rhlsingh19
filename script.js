d605cd14c42a482e9f9490a5c333f9aecede4bde
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-21 16:29:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function focusNext(input) {
+      if (input.value.length === 1) {
+        const nextInput = input.nextElementSibling;
+        if (nextInput) {
+          nextInput.focus();
+        }
+      }
+    }
+
+    function handleBackspace(input, event) {
+      if (event.key === 'Backspace' && input.value.length === 0) {
+        const prevInput = input.previousElementSibling;
+        if (prevInput) {
+          prevInput.focus();
+        }
+      }
+    }
